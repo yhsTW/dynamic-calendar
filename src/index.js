6118ecd076db20ba5479bacebcc6389044d087f3
DESKTOP-GUC6BIA\YHS <yhs0823@twinny.co.kr>
2019-10-01 14:54:43 +0900

    global.css 삭제

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import DynamicCalendar from './Components/DynamicCalendar';
-// import './global.css';
 
 ReactDOM.render(<DynamicCalendar />, document.getElementById('root'));
