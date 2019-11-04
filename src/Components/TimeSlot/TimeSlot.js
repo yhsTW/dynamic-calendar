7d1c25e7170b553ab947d437030e2b88dc248541
DESKTOP-GUC6BIA\YHS <yhs0823@twinny.co.kr>
2019-11-04 09:46:40 +0900

    TimeSlot prop-types 추가

--- src/Components/TimeSlot/TimeSlot.js
+++ src/Components/TimeSlot/TimeSlot.js
@@ -1,11 +1,12 @@
 import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
 import Label from '../Label';
 import BackgroundCell from '../BackgroundCell';
 import styles from './styles.css';
 import moment from 'moment';
 import { makeTimeFormat } from '../../utils/dateUtil';
 
-const TimeSlot = ({ items, week, customize, select, onSelectSlot, onSelectEvent, currentView }) => (
+const TimeSlot = ({ items, week, customize, select, onSelectSlot, currentView }) => (
     <div className={ styles.timeSlot }>
         { 
             !week && (
@@ -30,8 +31,7 @@ const TimeSlot = ({ items, week, customize, select, onSelectSlot, onSelectEvent,
                             return (
                                 <BackgroundCell key={ `${ newDate }_${ item.type }` } isMore={ false } isToday={ false } 
                                     item={{ ...item, date : newDate }} more={ 0 } openPopup={ () => {} } { ...select }
-                                    customize={ customize } onSelectSlot={ onSelectSlot }
-                                    onSelectEvent={ onSelectEvent } currentView={ currentView }
+                                    customize={ customize } onSelectSlot={ onSelectSlot } currentView={ currentView }
                                     useTime={ true } />
                             );
                         })
@@ -42,5 +42,37 @@ const TimeSlot = ({ items, week, customize, select, onSelectSlot, onSelectEvent,
     </div>
 );
 
+TimeSlot.propTypes = {
+    currentView : PropTypes.string,
+    customize : PropTypes.shape({
+        BackgroundCell : PropTypes.shape({
+            borderStyle : PropTypes.object.isRequired,
+            selectStyle : PropTypes.object.isRequired,
+            useBorder : PropTypes.bool.isRequired
+        })
+    }),
+    items : PropTypes.arrayOf(PropTypes.shape({
+        date : PropTypes.instanceOf(moment),
+        type : PropTypes.string.isRequired
+    }).isRequired).isRequired,
+    select : PropTypes.shape({
+        defaultSelectedDate : PropTypes.instanceOf(moment),
+        isSelecting : PropTypes.bool.isRequired,
+        lastSelectedDate : PropTypes.instanceOf(moment),
+        selectedEnd : PropTypes.instanceOf(moment),
+        selectedStart : PropTypes.instanceOf(moment),
+        setDefaultSelectedDate : PropTypes.func.isRequired,
+        setLastSelectedDate : PropTypes.func.isRequired,
+        setSelectedEnd : PropTypes.func.isRequired,
+        setSelectedStart : PropTypes.func.isRequired,
+        startSelecting : PropTypes.func.isRequired,
+        stopSelecting : PropTypes.func.isRequired
+    }).isRequired,
+    week : PropTypes.shape({
+        date : PropTypes.instanceOf(moment).isRequired,
+        type : PropTypes.string.isRequired
+    }),
+    onSelectSlot : PropTypes.func
+};
 
 export default TimeSlot;
