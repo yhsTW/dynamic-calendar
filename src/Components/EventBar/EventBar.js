9f6b743c5f83a8117224db12a34915f11d4f81b5
yhs <yhs0823@twinny.co.kr>
2019-12-23 15:25:00 +0900

    styles.css에 -로 되어있던 부분 카멜케이스로 수정

--- src/Components/EventBar/EventBar.js
+++ src/Components/EventBar/EventBar.js
@@ -4,8 +4,8 @@ import styles from './styles.css';
 
 const EventBar = ({ useTime, time, event }) => (
     <Fragment>
-        { useTime && <Label className={ styles.timeLabel } text={ time } /> }
-        <Label text={ event.title } />
+        { useTime && <Label className={ `${ styles.timeLabel } ${ styles.ellipsisText }` } text={ time } /> }
+        <Label className={ !useTime ? styles.ellipsisText : '' } text={ event.title } />
     </Fragment>
 );
 
