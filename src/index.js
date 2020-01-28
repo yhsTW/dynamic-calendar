52dc264afc2a4e3cfd666606dee24e36d5f913ac
YHS <yhs0823@twinny.co.kr>
2020-01-28 15:41:13 +0900

    필요없는 코드 삭제

--- src/index.js
+++ src/index.js
@@ -3,5 +3,4 @@ import ReactDOM from 'react-dom';
 import DynamicCalendar from './Components/DynamicCalendar';
 import './global.css';
 
-ReactDOM.render(<DynamicCalendar customize={{ View : { BackgroundCell : { useBorder : true, borderStyle : { border : '1px solid #DDD'} } } }} />, document.getElementById('root'));
-// ReactDOM.render(<DynamicCalendar />, document.getElementById('root'));
+ReactDOM.render(<DynamicCalendar />, document.getElementById('root'));
