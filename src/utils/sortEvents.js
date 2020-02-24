1ce800ab1145b9f40c581fab1c6787d6fc89e5bd
YHS <yhs0823@twinny.co.kr>
2020-02-24 14:03:34 +0900

    sort 방식 수정

--- src/utils/sortEvents.js
+++ src/utils/sortEvents.js
@@ -30,14 +30,16 @@ const sortEvents = (events, { start : startKey, end : endKey, allDay : allDayKey
             } else if(mBStart.isBefore(mAStart, 'date')) {
                 // b의 시작날짜가 더 빠를 경우 b가 우선순위
                 return 1;
+            } else {
+                return 0;
             }
         } else {
             // a, b의 시작 날짜가 같을 때
             if(mAEnd.isAfter(mBEnd, 'date')) {
-                // a의 종료날짜가 더 이후에 있을 때
+                // a의 종료날짜가 더 이후에 있을 경우 a가 우선순위
                 return -1;
             } else if(mBEnd.isAfter(mAEnd, 'date')) {
-                // b의 종료날짜가 더 이후에 있을 때
+                // b의 종료날짜가 더 이후에 있을 경우 b가 우선순위
                 return 1;
             } else {
                 // a, b의 종료날짜가 모두 같을 때
@@ -49,18 +51,18 @@ const sortEvents = (events, { start : startKey, end : endKey, allDay : allDayKey
                     return 1;
                 } else {
                     // 둘 다 종일/비종일일 때
-                    if(mAEnd.isSameOrAfter(mBEnd)) {
+                    if(mAEnd.isAfter(mBEnd)) {
                         // a가 종료 시간이 더 늦을 경우 a가 우선순위
                         return -1
-                    } else {
+                    } else if(mBEnd.isAfter(mAEnd)) {
                         // b가 종료 시간이 더 늦을 경우 b가 우선순위
                         return 1;
+                    } else {
+                        return 0;
                     }
                 }
             }
         }
-
-        return 0;
     });
 
     return sort;
